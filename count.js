8cc7f04ab059ebbb7a7b9d426fc5293b2f30527c
ethanteo99 <ethanteo99@gmail.com>
2017-11-02 09:51:52 -0700

    add filters

--- count.js
+++ count.js
@@ -1,4 +1,25 @@
-chrome.tabs.query({}, function(tabs){
-    var count = tabs.length;
-    chrome.browserAction.setBadgeText({text: count.toString()});
+// https://developer.chrome.com/extensions/tabs#method-query
+function applyFilter(filter){
+    if (filter === 'all'){
+        queryInfo = {};
+    } else if (filter === 'window') {
+        queryInfo = {currentWindow: true};
+    } else if (filter === 'audible'){
+        queryInfo = {audible: true};
+    }
+
+    chrome.tabs.query(queryInfo, function(tabs){
+        var count = tabs.length;
+        chrome.browserAction.setBadgeText({text: count.toString()});
+    });
+}
+
+
+// Default filter
+applyFilter('all')
+
+// Update filter when dropdown value is changed.
+document.addEventListener('DOMContentLoaded', () => {
+    filter = document.getElementById("dropdown")
+    filter.onchange = function () {applyFilter(filter.value);};
 });
