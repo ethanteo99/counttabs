00fb44b6f63492d028a2f7c0b31aa307a2758d9c
ethanteo99 <ethanteo99@gmail.com>
2017-11-04 16:25:29 -0700

    simplified interface, update in real time

--- count.js
+++ count.js
@@ -1,25 +1,11 @@
 // https://developer.chrome.com/extensions/tabs#method-query
-function applyFilter(filter){
-    if (filter === 'all'){
-        queryInfo = {};
-    } else if (filter === 'window') {
-        queryInfo = {currentWindow: true};
-    } else if (filter === 'audible'){
-        queryInfo = {audible: true};
-    }
-
-    chrome.tabs.query(queryInfo, function(tabs){
+function applyFilter(){
+    chrome.tabs.query({}, function(tabs){
         var count = tabs.length;
         chrome.browserAction.setBadgeText({text: count.toString()});
     });
 }
 
-
-// Default filter
-applyFilter('all')
-
-// Update filter when dropdown value is changed.
-document.addEventListener('DOMContentLoaded', () => {
-    filter = document.getElementById("dropdown")
-    filter.onchange = function () {applyFilter(filter.value);};
-});
+setInterval(function(){
+    applyFilter();
+}, 100);
